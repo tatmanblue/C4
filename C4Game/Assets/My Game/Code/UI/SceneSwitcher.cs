8dd9dc0314ee6ee035845f734e0e40482bb610df
tatmanblue <Matt.Raffel@gmail.com>
2020-11-12 11:23:07 -0700

    first rough pass at menu screen
    Signed-off-by: tatmanblue <Matt.Raffel@gmail.com>

---
+++ C4Game/Assets/My Game/Code/UI/SceneSwitcher.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using CornTheory;
+
+/*
+using CornTheory.Missions;
+using CornTheory.Player;
+*/
+
+namespace CornTheory.UI
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+        [SerializeField] Texture2D defaultCursor = null;
+        [SerializeField] Vector2 defaultCursorHotspot = new Vector2(0, 0);
+        [SerializeField] Texture2D busyCursor = null;
+        [SerializeField] Vector2 busyCursorHotspot = new Vector2(0, 0);
+
+        [Header("Scene Change Options")]
+        [SerializeField] bool showMissionsSceneStart = false;
+        [SerializeField] float showDelay = 0.75f;
+        [SerializeField] Material skyBox = null;
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            print("scene loaded: " + scene.name);
+            SetSkyBox(scene);
+            InvokeMissionPopup(scene);
+            SetCursor();
+        }
+
+        private void SetSkyBox(Scene scene)
+        {
+            if (null == skyBox)
+                return;
+
+            RenderSettings.skybox = skyBox;
+        }
+
+        private void InvokeMissionPopup(Scene scene)
+        {
+            print(string.Format("scene name found: {0}", scene.name));
+            print(string.Format("Is scene {0} = {1}", Constants.MainWorldScene, scene.name == Constants.MainWorldScene));
+            print(string.Format("Is scene {0} = {1}", Constants.MainWorldSceneExperimental, scene.name == Constants.MainWorldSceneExperimental));
+            // TODO:  this is hooky when playing around
+            if ((scene.name == Constants.MainWorldScene && true == showMissionsSceneStart)
+                || (scene.name == Constants.MainWorldSceneExperimental && true == showMissionsSceneStart)
+                || scene.name == Constants.DevPlayArenaScene)
+            {
+                // PlayerState.Instance.GameState = GameUIState.InWorld;
+                // var missionManager = FindObjectOfType<MissionManager>();
+                // missionManager.Invoke("SceneOpened", showDelay);
+            }
+        }
+
+        private void SetCursor()
+        {
+            if (null == defaultCursor)
+                return;
+
+            Cursor.SetCursor(defaultCursor, defaultCursorHotspot, CursorMode.Auto);
+        }
+
+        private void Awake()
+        {
+            print("SceneSwitcher.awake() called");
+            print("adding event handler to SceneManager");
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        void Start() { }
+
+        // called when the game is terminated
+        void OnDisable()
+        {
+            print("SceneSwitcher.OnDisable() called");
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        public void GotoMainMenu()
+        {
+            Cursor.SetCursor(busyCursor, busyCursorHotspot, CursorMode.Auto);
+            // Changing state here assuming that we are coming from the world scenes
+            // PlayerState.Instance.GameState = GameUIState.AtMain;
+            SceneManager.LoadScene(Constants.OpenMenuScene);
+        }
+
+        public void GotoMainScene()
+        {
+            // TODO: eval if it is right to not change state here
+            // DONT CHANGE STATE HERE
+            SceneManager.LoadScene(Constants.MainWorldScene);
+        }
+
+        public void GotoPlayArena()
+        {
+            // TODO: this is only a dev thing and shouldn't be in production
+            SceneManager.LoadScene(Constants.DevPlayArenaScene);
+        }
+
+        public void GotoOpenWorldPrototype()
+        {
+            SceneManager.LoadScene(Constants.MainWorldSceneExperimental);
+        }
+
+        public void Quit()
+        {
+            Cursor.SetCursor(busyCursor, busyCursorHotspot, CursorMode.Auto);
+            Application.Quit();
+        }
+    }
+}
--- Docs/ASSET LIST.md
+++ Docs/ASSET LIST.md
@@ -1,6 +1,12 @@
 # Unity Asset Library
+TextMesh Pro
 
 
+# From the asset store
+[UI Builder](https://assetstore.unity.com/packages/tools/gui/ui-builder-29757)  
+
+
+# TBD
 [Corn Stalk Package](https://assetstore.unity.com/packages/3d/vegetation/speedtree/corn-stalk-package-32282)  
 [Dialogue System] (https://assetstore.unity.com/packages/tools/terrain/landscape-builder-55463)  
 [Easy Roads 3d](https://assetstore.unity.com/packages/tools/terrain/easyroads3d-pro-v3-469)  
