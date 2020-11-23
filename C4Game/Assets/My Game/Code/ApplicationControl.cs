77178f4e2acda8bef3923fdb03e9912641ecad1e
matthew raffel <matt.raffel@gmail.com>
2020-11-23 10:58:40 -0700

    working on screen resolution and layout tests
    Signed-off-by: matthew raffel <matt.raffel@gmail.com>

---
+++ C4Game/Assets/My Game/Code/ApplicationControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CornTheory
+{
+    public class ApplicationControl : MonoBehaviour
+    {
+        public void Quit(int exitCode = 0)
+        {
+            Application.Quit(exitCode);
+        }
+
+        public void Set640x480(bool fullScreen = true)
+        {
+            Screen.SetResolution(640, 480, fullScreen);
+        }
+        
+        public void Set1024x768(bool fullScreen = true)
+        {
+            Screen.SetResolution(1024, 768, fullScreen);
+        }
+    }
+}
--- Docs/USEFUL WEBSITES.md
+++ Docs/USEFUL WEBSITES.md
@@ -7,8 +7,8 @@ https://cosminmirza.bandcamp.com/album/free-random-music-pack4
 ## images
 https://www.shutterstock.com/collections
 https://www.istockphoto.com/collaboration/boards/CWsApVFdaUaZEEltNP6pgw
-
+https://stock.adobe.com/Library/554e1379-f0a6-408f-99c0-e613b29d65c4
 
 ## how to
 https://docs.unity3d.com/ScriptReference/Resources.Load.html
-
+http://www.aclockworkberry.com/managing-screen-resolution-and-aspect-ratio-in-unity-3d/
