b4a17ccf25a1b421262403c3e47886d61990afde
matthew raffel <matt.raffel@gmail.com>
2020-11-24 12:38:52 -0700

    fading works but not perfected
    Signed-off-by: matthew raffel <matt.raffel@gmail.com>

--- C4Game/Assets/My Game/Code/UI/AlphaFader.cs
+++ C4Game/Assets/My Game/Code/UI/AlphaFader.cs
@@ -19,20 +19,23 @@ namespace CornTheory.UI
         /// the number if milliseconds to incrementally adjust alpha.
         /// </summary>
         [SerializeField] private int DurationMS;
+        /// <summary>
+        /// when true, the fading start immediately
+        /// </summary>
+        [SerializeField] private bool AutoStart = false;
 
         private DateTime startedAt;
         private DateTime lastUpdateAt;
         private float currentAlpha = 0.0F;
         private float alphaDelta = 0.0F;
-        private bool run = false;
+        private bool run = true;
+        private Color originalColor;
 
         public void StartFading(float alphaAdjustTo, int durationMS)
         {
-            // 1 - calculate the amount of adjustment to make
-            //       formula of # milliseconds and total AlphaAdjustment
-            //        eg:  10MS and 10 alpha would be 1 alpha per 1 MS
-            // 2 - start the process
-            Debug.Log("TODO");
+            AlphaAdjustTo = alphaAdjustTo;
+            DurationMS = durationMS;
+            run = true;
         }
 
         public void StartFading()
@@ -42,11 +45,16 @@ namespace CornTheory.UI
 
         private void Start()
         {
+            // dont let any frames start until we did our math
+            run = false;
             lastUpdateAt = DateTime.Now;
             startedAt = lastUpdateAt;
             alphaDelta = AlphaAdjustTo / DurationMS;
-            // Color color = Item.material.color;
-            // Item.material.color = new Color(color.a, color.g, color.b, 0);
+            originalColor = Item.color;
+            Item.color = new Color(originalColor.a, originalColor.g, originalColor.b, 0);
+            
+            // if autostart is true, then FixedUpdate will start doing the work 
+            run = AutoStart;
         }
 
         private void FixedUpdate()
@@ -60,15 +68,16 @@ namespace CornTheory.UI
             
             if (delta.Milliseconds >= 0.01F)
             {
-                Color color = Item.material.color;
+                Color color = Item.color;
                 if (color.a >= 1.0F)
                 {
+                    Debug.Log($"Started at {startedAt} and finished at {lastUpdateAt}");
+                    Item.color = originalColor;
                     run = false;
                     return;
                 }
-                Color withNewAlpha = new Color(color.a, color.g, color.b, color.a + alphaDelta);
-                Debug.Log($"changing alpha to {withNewAlpha.a} from {color.a}");
-                Item.material.color = withNewAlpha;
+                Color withNewAlpha = new Color(color.a, color.g, color.b, color.a + (alphaDelta * delta.Milliseconds));
+                Item.color = withNewAlpha;
             }
         }
     }
--- C4Game/Assets/My Game/Code/UI/IncomingTextTypingHistoryRunner.cs
+++ C4Game/Assets/My Game/Code/UI/IncomingTextTypingHistoryRunner.cs
@@ -17,7 +17,11 @@ namespace CornTheory.UI
         /// </summary>
         [SerializeField] private GameObject ScrollView = null;
 
+        [SerializeField] private AlphaFader Fader = null;
+
         private CornTheory.UI.ListCreator listCreator;
+        private int messagesReceived = 0;
+        private bool fadingStarted = false;
 
         private void Start()
         {
@@ -33,14 +37,31 @@ namespace CornTheory.UI
         
         private IEnumerator StartTypingText(TypeableTextLine item, IncomingTextTypingHistoryItem uiItem)
         {
+            // start by allowing the incoming message indicator to blink for duration
             float waitMS = uiItem.WaitMS / 1000F;
             yield return new WaitForSeconds(waitMS);
+            
+            // duration has expired, replace with text
+            // TODO: should sound play first?
+            listCreator.PlayReceivedIndicatorSound();
+            yield return null;
+            
             uiItem.WaitImage.SetActive(false);
             uiItem.Who.text = item.ActorId;
             uiItem.Said.text = item.Text;
-            listCreator.PlayReceivedIndicatorSound();
+            messagesReceived++;
+            yield return null;
+            
+            // notify everyone
             CompletedTextTypingAction action = OnTextTypingCompleted;
             if (null != action) action(item);
+            
+            // and start the fading of the controls
+            if ((fadingStarted == false) && (messagesReceived > 1) && (null != Fader))
+            {
+                fadingStarted = true;
+                Fader.StartFading();
+            }
         }
     }
 }
